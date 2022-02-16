d19e725a736a8398d1c74a0443f62e72bee867ce
Shivam Rajput <shivam.rajput@thoughtworks.com>
2022-02-16 08:47:46 +0530

    updated theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,21 +1,5 @@
-import { blue, deepPurple } from '@mui/material/colors'
 import { createTheme } from '@mui/material'
 
-const theme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      extraLight: deepPurple[100],
-      light: deepPurple[300],
-      main: deepPurple[600],
-      dark: deepPurple[900]
-    },
-    secondary: {
-      extraLight: blue[100],
-      light: blue[300],
-      main: blue[600]
-    }
-  }
-})
+const theme = createTheme({})
 
 export default theme
