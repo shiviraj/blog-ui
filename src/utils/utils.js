a4a40d9cf83fbf23ade80ce4c6ee91c5a9f8bd42
Shivam Rajput <shivam.rajput@thoughtworks.com>
2022-02-23 13:07:25 +0530

    updated timezone as local date time.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,7 @@
 import moment from 'moment'
 
-const formatDate = date => {
-  return moment(date).format('MMM DD, YYYY')
-}
+const formatDate = (date) => moment(date).local().format('MMM DD, YYYY')
 
-const formatDateTime = date => {
-  return moment(date).format('MMM DD, YYYY hh:mm A')
-}
+const formatDateTime = (date) => moment(date).local().format('MMM DD, YYYY hh:mm A')
 
 export { formatDate, formatDateTime }
